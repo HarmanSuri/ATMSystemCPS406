30c8b78b1901802f869435699825bdabc34b7c7a
RohitBhogal <128306475+RohitBhogal@users.noreply.github.com>
2023-03-26 10:19:57 -0400

    Add files via upload

--- tests/SavingsAccountTest.java
+++ tests/SavingsAccountTest.java
@@ -5,6 +5,69 @@ import org.junit.Test;
 import AtmSystem.SavingsAccount;
 
 public class SavingsAccountTest {
+    @Test
+    public void testGetBalance1() {
+        SavingsAccount account = new SavingsAccount(200, 1, 500, 0.08, 30);
+        assertEquals(200, account.getBalance(), 0.001);
+    }
+
+    @Test
+    public void testGetBalance2() {
+        SavingsAccount account = new SavingsAccount(500, 1, 500, 0.08, 30);
+        assertEquals(500, account.getBalance(), 0.001);
+    }
+
+    @Test
+    public void testGetAccountID1() {
+        SavingsAccount account = new SavingsAccount(200, 1, 500, 0.08, 30);
+        assertEquals(1, account.getAccID());
+    }
+
+    @Test
+    public void testGetAccountID2() {
+        SavingsAccount account = new SavingsAccount(200, 5, 500, 0.08, 30);
+        assertEquals(5, account.getAccID());
+    }
+
+    @Test
+    public void testGetWithdrawLimit1() {
+        SavingsAccount account = new SavingsAccount(200, 1, 500, 0.08, 30);
+        assertEquals(500, account.getWithdrawLimit(), 0.001);
+    }
+
+    @Test
+    public void testGetWithdrawLimit2() {
+        SavingsAccount account = new SavingsAccount(200, 1, 1000, 0.08, 30);
+        assertEquals(1000, account.getWithdrawLimit(), 0.001);
+    }
+
+    @Test
+    public void testSetBalance1() {
+        SavingsAccount account = new SavingsAccount(200, 1, 1000, 0.08, 30);
+        account.setBalance(500);
+        assertEquals(500, account.getBalance(), 0.001);
+    }
+
+    @Test
+    public void testSetBalance2() {
+        SavingsAccount account = new SavingsAccount(200, 1, 1000, 0.08, 30);
+        account.setBalance(1000);
+        assertEquals(1000, account.getBalance(), 0.001);
+    }
+
+    @Test
+    public void testSetWithdrawLimit1() {
+        SavingsAccount account = new SavingsAccount(200, 1, 1000, 0.08, 30);
+        account.setWithdrawLimit(500);
+        assertEquals(500, account.getWithdrawLimit(), 0.001);
+    }
+
+    @Test
+    public void testSetWithdrawLimit2() {
+        SavingsAccount account = new SavingsAccount(200, 1, 1000, 0.08, 30);
+        account.setWithdrawLimit(1200);
+        assertEquals(1200, account.getWithdrawLimit(), 0.001);
+    }
 
     @Test
     public void testDeposit1() {
